961b247e47dd9ad14ffbedabe6bcd1bea041fdc9
tahomaspice <124075982+tahomaspice@users.noreply.github.com>
2023-03-04 14:57:04 -0800

    fix menus navbar final

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,14 @@
-import { Box, Flex, HStack, Link, IconButton, Icon, Text, useDisclosure, Button, Stack, useColorModeValue as mode, 
+import { Box, Flex, HStack, Link, IconButton, Icon, Text, useDisclosure, Button, Stack, useColorModeValue, 
     useColorMode, useToast, MenuButton, MenuDivider, Menu, MenuList, MenuItem, } from '@chakra-ui/react';
 import { Link as ReactLink } from 'react-router-dom';
 import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon, ChevronDownIcon } from '@chakra-ui/icons';
 import { CgProfile } from 'react-icons/cg';
-import { MdLocalShipping, MdLogout, MdOutlineAdminPanelSettings, MdShoppingBag } from 'react-icons/md';
+import { MdLocalShipping, MdLogout, MdOutlineAdminPanelSettings } from 'react-icons/md';
 import {FiShoppingCart} from 'react-icons/fi';
 import { GiTechnoHeart } from 'react-icons/gi';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../redux/actions/userActions';
-
 
 const ShoppingCartIcon = () => {
     const cartInfo = useSelector((state) => state.cart);
@@ -21,22 +20,25 @@ const ShoppingCartIcon = () => {
             </Text>
             <Icon ml='-1.5' as={FiShoppingCart} h='4' w='7' alignSelf='center' />
             Cart
+            
         </Flex>
+
+        
+
     );
 };
 
+
+
 const links = [
-    { linkName: 'All Products', path: '/products'},
-    {MenuDivider},
+    { linkName: 'Products', path: '/products'},
     { linkName: 'Hot Sauce', path: '/products'},
     { linkName: 'BBQ Sauce', path: '/products'},
     { linkName: 'Salsa', path: '/products'},
     { linkName: 'Gifts', path: '/products'},
-    {MenuDivider},
+    { linkName: 'Blog', path: '/products'},
     { linkName: <ShoppingCartIcon />, path: '/cart'},
-   
 ];
-
 const NavLink = ({ path, children }) => (
     <Link 
         as={ReactLink} 
@@ -44,7 +46,7 @@ const NavLink = ({ path, children }) => (
         px={2} 
         py={2} 
         rounded='md' 
-        _hover={{ textDecoration: 'none', bg: mode('gray.200', 'gray.700')}}>
+        _hover={{ textDecoration: 'none', bg: useColorModeValue('gray.200', 'gray.700')}}>
     {children}
     </Link>
 );
@@ -64,7 +66,7 @@ const Navbar = () => {
 };
 
 return (
-        <Box bg={mode('gray.100', 'gray.900')} px={4}>
+        <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
             <Flex h={16} alignItems='center' justifyContent='space-between'>
                 <IconButton 
                 size='md' 
@@ -78,46 +80,24 @@ return (
                     onMouseLeave={() => setIsHovering(false)}>
                         <Flex alignItems='center'>
                             <Icon as={GiTechnoHeart} h={6} w={6} color={isHovering ? 'cyan.400' : 'orange.400'} />
-                            <Text px='2' fontWeight='extrabold'>Tahoma Spice Company</Text>
+                            <Text fontWeight='extrabold'>Tahoma Spice Company</Text>
                         </Flex>
                     </Link>
-       
-
-                 <HStack>        
-                        {userInfo.isAdmin === true && (
-                            <>
-                            <MenuDivider />
-                                <MenuItem as={ReactLink} to={'/admin-console'}>
-                                    <MdOutlineAdminPanelSettings />
-                                    <Text ml='2'>Admin Console</Text>
-                                </MenuItem>
-                            
-                            
-                            </>
-                        )}
-                </HStack>
-                 
                     <HStack as='nav' spacing={4} display={{base: 'none', md: 'flex'}}>
                         {links.map((link) => (
                             <NavLink key={link.linkName} path={link.path}>
                                 {link.linkName}
                             </NavLink>
-                        ))}                    
-                                  
-                     </HStack>
-             </HStack> 
+                        ))}
+                    </HStack>
+                </HStack>  
 
-                
-
-
-                
                 <Flex alignItems='center'>
                     <NavLink>
                     <Icon as={colorMode === 'light' ? MoonIcon: SunIcon} alignSelf='center' 
                             onClick={() => toggleColorMode()} 
                     />
                     </NavLink>
-                
                 
                 {userInfo ? (
                 
@@ -166,25 +146,22 @@ return (
                     </>
                       
                 )}
-                </Flex>
-            </Flex>
-      
-        
-        {isOpen ? (<Box pb={4} display={{ md: "none"}}>
+                
+        </Flex>
+        </Flex>
+        {isOpen ? <Box pb={4} display={{ md: "none"}}>
                 <Stack as="nav" spacing={4}>
                   {links.map((link) => (
                             <NavLink key={link.linkName} path={link.path}>
                                 {link.linkName}
                             </NavLink>
-                            
                         ))} 
                         <NavLink key='sign up' path='/registration'>Sign Up</NavLink> 
                 </Stack>            
-        </Box>
-        )  : null}
+        </Box> : null}
         
       </Box>
-      
+
     );
 };
 
